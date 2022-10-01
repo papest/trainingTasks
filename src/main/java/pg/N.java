0094e65ebd252d83b27e3096b3379dd8ed9c2ff7
Epap <peia@inbox.ru>
2022-10-01 18:41:17 +0300

    Nineteenth commit

--- src/main/java/pg/N.java
+++ src/main/java/pg/N.java
@@ -3,41 +3,86 @@ package pg;
 import java.util.*;
 
 ///**
- class Node {
+class Node {
 
- public final int val;
- public final List<Node> neighbours;
+    public final int val;
+    public final List<Node> neighbours;
 
- public Node(int val) {
- this.val = val;
- this.neighbours = new ArrayList<>();
- }
- }
- //*/
+    public Node(int val) {
+        this.val = val;
+        this.neighbours = new ArrayList<>();
+    }
+}
+//*/
 
 public class N {
-     //Атака клонов
+    //Атака клонов
     static Node[] nodes = new Node[10000];
+    static Node[] nodes1 = new Node[10000];
     static int pos = 0;
 
+    public static void main(String[] args) {
+        Node node1 = new Node(3);
+        Node node2 = new Node(1);
+        Node node3 = new Node(2);
+        node1.neighbours.add(node2);
+        node2.neighbours.add(node1);
+        node1.neighbours.add(node3);
+        node3.neighbours.add(node1);
+        node2.neighbours.add(node3);
+        node3.neighbours.add(node2);
+        Node clone = cloneGraph(node1);
+        int n = 5;
 
-    public static Node cloneGraph(Node node) {
+    }
 
-        int nodeIndex = -1;
-
-        for (int i = 0; i < pos; i++) {
-            if (nodes[i].val == node.val) {
-                nodeIndex = i;
-                break;
+    private static int binarySearch(Node node) {
+        int left = 0;
+        int right = pos;
+        int mid;
+        while (left < right) {
+            mid = (left + right) >> 1;
+            if (node.val > nodes[mid].val) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        if (nodeIndex != -1) {
-            return nodes[nodeIndex];
+        return left;
+    }
+
+    private static void addNode(Node node) {
+        int index = binarySearch(node);
+        if (index < pos && nodes[index].val == node.val) {
+            return;
+        }
+        if (index < pos) {
+            System.arraycopy(nodes, index, nodes, index + 1, pos - index);
+        }
+        nodes[index] = node;
+        pos++;
+        for (int i = 0, size = pos; i < size; i++) {
+            nodes[i].neighbours.forEach(node1 -> addNode(node1));
         }
 
-        Node finalClone = new Node(node.val);
-        node.neighbours.forEach(node1 -> finalClone.neighbours.add(N.cloneGraph(node1)));
-        nodes[pos++] = finalClone;
-        return finalClone;
+    }
+
+    public static Node cloneGraph(Node node) {
+        nodes[pos++] = node;
+        node.neighbours.forEach(node1 -> addNode(node1));
+        int startNodeIndex = binarySearch(node);
+        for (int i = 0; i < pos; i++) {
+            nodes1[i] = new Node(nodes[i].val);
+        }
+        for (int i = 0; i < pos; i++) {
+
+            for (Node node1 : nodes[i].neighbours) {
+                int ind = binarySearch(node1);
+                nodes1[i].neighbours.add(nodes1[ind]);
+            }
+        }
+
+
+        return nodes1[startNodeIndex];
     }
 }
